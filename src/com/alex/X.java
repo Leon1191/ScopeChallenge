6ab55bcecebc89e964879534fb2ac8f5c4893caa
Leon <stbirb@yandex.ru>
2019-06-13 17:26:52 +0300

    Initial commit

---
+++ src/com/alex/X.java
@@ -0,0 +1,14 @@
+package com.alex;
+
+public class X {
+    private int x;
+
+
+    public X(int x) {
+
+        for (this.x = 1; this.x < 13; this.x++) {
+            System.out.println(x * this.x);
+
+        }
+    }
+}
